547064bba558dbd0ec0ce32e7a2f306ff0b1e722
Igor <86882588+Ray-Janson@users.noreply.github.com>
2021-08-07 03:19:00 +0700

    Tabsssssssssssss

---
+++ src/js/TabFilter.js
@@ -0,0 +1,11 @@
+"use strict"
+
+export class TabFilter {
+    constructor(tabsPanel, itemsWrapper) {
+        this.tabsPanel = tabsPanel
+        this.itemsWrapper = itemsWrapper
+    }
+    init() {
+        console.log("sdfs")
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 import { slider } from "./slider.js"
 import { mobileSideMenu } from "./mobileSideMenu.js"
+import { TabFilter } from "./TabFilter.js"
+
 
 
 const sliderWrapper = document.getElementById("slider-wrapper")
@@ -9,8 +11,16 @@ const s = new slider(sliderWrapper, "slider-btn-disabled", "slider-btn-active")
 s.init()
 
 
+
 const sideMenuBtnOpen = document.getElementById("side-menu-btn-open")
 const sideMenuBtnClose = document.getElementById("side-menu-btn-close")
 const sideMenuMobile = document.getElementById("side-menu-mobile")
 const sideMenu = new mobileSideMenu(sideMenuBtnOpen, sideMenuBtnClose, sideMenuMobile)
 sideMenu.init()
+
+
+
+const tabsPanel = document.getElementById("tabs-panel")
+const itemsWrapper = document.getElementById("items-wrapper")
+const tf = new TabFilter(tabsPanel, itemsWrapper)
+tf.init()
